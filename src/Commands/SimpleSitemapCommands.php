c5d9beb12099e4814e1841b3fdd47e444e21524e
badjava <badjava@83372.no-reply.drupal.org>
2017-10-05 00:25:05 +0200

    Issue #2912684 by badjava: Add Drush 9 command support

---
+++ src/Commands/SimpleSitemapCommands.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\simple_sitemap\Commands;
+
+use Drupal\simple_sitemap\Simplesitemap;
+use Drush\Commands\DrushCommands;
+
+/**
+ * Class SimpleSitemapCommands
+ * @package Drupal\simple_sitemap\Commands
+ */
+class SimpleSitemapCommands extends DrushCommands {
+
+  /**
+   * @var \Drupal\simple_sitemap\Simplesitemap
+   */
+  protected $generator;
+
+  /**
+   * SimpleSitemapCommands constructor.
+   * @param \Drupal\simple_sitemap\Simplesitemap $generator
+   */
+  public function __construct(Simplesitemap $generator) {
+    $this->generator = $generator;
+  }
+
+  /**
+   * Regenerate XML sitemaps for all languages according to the module settings.
+   *
+   * @command simple-sitemap:generate
+   * @aliases ss:generate, ssg
+   */
+  public function generate() {
+    $this->generator->generateSitemap('drush');
+  }
+
+}
